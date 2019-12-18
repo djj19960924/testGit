2fe184b1f4bf651e50effb38b7f5d5187bc54117
djj <djj@gitlab.com>
2019-12-18 10:12:26 +0800

    12.18

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+export default {
+  name: 'App',
+  mounted(){
+    this.$axios({
+      // Headers:{
+      //   'Content-Type':"application/json;charset=utf-8",
+      // },
+      method:'get',
+      url:'/api/users'
+    }).then((response) => {
+      console.log(response.data)
+    }).catch((response)=>{
+      console.log(response.data);
+    })
+  }
+}
+</script>
